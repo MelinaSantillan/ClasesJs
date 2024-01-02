4b28d4d60e402c7ff620b50d142631f127bf211d
MelinaSantillan <santillan.melina.dh@gmail.com>
2024-01-02 20:09:58 -0300

    vimos variables, mensajes por consola y mensajes de alerta

---
+++ js/primeraParte.js
@@ -0,0 +1,42 @@
+//esto es un comentario de una sola linea
+
+/**
+ * esto es un comentario de varias lineas
+ */
+//esto imprime un mensaje en consola
+console.log("hola mundo");
+console.log("Esto es otro mensaje mostrado por consola");
+
+//variables
+
+/**sintaxis
+ * identificador del tipo de variable+ nombre de la variable= valor
+ */
+
+let mensaje = "este es el valor inicial de mi variable";
+let edad = 31;
+let nombre = "Melina";
+
+console.log(mensaje);
+console.log(edad);
+console.log(nombre);
+
+mensaje="este es el mensaje mof¿dificado";
+console.log(mensaje);
+// con esto escribo en el documento html
+document.write(mensaje);
+document.write("<br/>");
+document.writeln(nombre);
+document.write("<br/>")
+document.writeln(edad);
+//con esto muestro un mensaje de alerta en un pop
+//alert("Esto es un menaje de alerta")
+//con esto le pido al usuario que ingrese un dato
+//prompt("por favor ingrese su nombre")
+
+let datoIngresado = prompt ("por favor ingrese su nombre");
+alert("usted ingreso lo siguiente: " + datoIngresado)
+
+
+/**tarea: leer documentacion Js campus */
+
